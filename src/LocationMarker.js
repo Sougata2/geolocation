5832ecf3454643fca864deb439f66752ad17a4cf
sougata <sougata19582001@gmail.com>
2024-04-30 00:19:10 +0530

    fly to the marker

--- src/LocationMarker.js
+++ src/LocationMarker.js
@@ -10,26 +10,29 @@ function LocationMarker() {
     },
     locationfound(e) {
       setPosition(e.latlng);
-      map.flyTo(e.latlng, map.getZoom(28));
+      map.flyTo(e.latlng, 10);
     },
   });
 
-  useEffect(function () {
-    const interval = setInterval(function () {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-          setPosition({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
+  useEffect(
+    function () {
+      const interval = setInterval(function () {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function (position) {
+            setPosition({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            });
           });
-        });
-        map.getCenter(position);
-      }
-    }, 3000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+          if (position) map.flyTo(position, 16);
+        }
+      }, 3000);
+      return () => {
+        clearInterval(interval);
+      };
+    },
+    [map, position]
+  );
 
   return position === null ? null : (
     <Marker position={position}>
